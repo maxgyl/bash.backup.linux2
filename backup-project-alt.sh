2ad57f11473bccc92e2e7d2e92adf7abdaaf5fb5
Marwal1987 <73877564+Marwal1987@users.noreply.github.com>
2020-11-14 09:30:09 +0100

    Create backup-project-alt.sh

---
+++ backup-project-alt.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#	backup-project-alt.sh
+
+
+# Backup to NFS mount script.
+# Encryption using openssl.
+
+
+# What to backup
+backup_files="/var/spool/mail /etc /root /boot /opt"
+
+# Where to backup to.
+dest="/mnt/backup"
+
+# Create archive filename.
+day=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+# Print start status msg
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+
+# Copy script to cron daily
+sudo cp backup-project-alt.sh /etc/cron.daily/backup-project-alt.sh
+
+#	Backup the files with Tar and encrypt with openssl
+tar czf $dest/"$archive_file" "$backup_files" | openssl enc -e -aes256 -out backup_secured.tar.gz
+
+# Print end status msg.
+echo
+echo "Backup finished"
+date
+
+exit 0
